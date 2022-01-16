7e9a9fda6b7fbabcb3fa606a66626776ac9993d2
Glendon Chin <glendonchin@gmail.com>
2022-01-16 16:06:23 -0500

    Add walkthrough explanation

--- Leetcode/Search_Insert_Position.py
+++ Leetcode/Search_Insert_Position.py
@@ -5,16 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        low=0
-        high=len(nums)-1
+        #Lower and upper bounds
+        low, high = 0, len(nums)-1
+        
+        #Traverse search space
         while low<=high:
+            
+            #Get midpoint *use floor to get whole integer
             mid=(low+high)//2
             
+            #If target already exists, return index
             if nums[mid]==target:
                 return mid
+            
+            #If target is larger than mid indexed value, search right
             elif nums[mid]<target:
                 low=mid+1
+                
+            #If target is smaller/equal to mid indexed value, search left
             else:
                 high=mid-1
+        
         return high+1
         #Runtime: 28ms
