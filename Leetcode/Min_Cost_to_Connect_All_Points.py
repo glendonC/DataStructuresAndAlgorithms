c841c70638c30bf715cfa2ae7665492aae037646
Glendon Chin <glendonchin@gmail.com>
2022-02-26 18:36:56 -0500

    1584

---
+++ Leetcode/Min_Cost_to_Connect_All_Points.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def minCostConnectPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        N = len(points)
+        
+        #for i in range(N), map i to an empty list
+        adj = {i:[] for i in range(N)} #i: lits of [costm node]
+        for i in range(N):
+            x1, y1 = points[i]
+            #compare it to every other point in the graph
+            for j in range(i+1, N):
+                x2,y2=points[j]
+                dist = abs(x1-x2)+abs(y1-y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+        
+        #Prim's
+        res = 0
+        visit = set()
+        minH = [[0,0]] # [cost, point]
+        while len(visit)<N:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            #if it hasn't been popped
+            res+=cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
+                
+            
+                
